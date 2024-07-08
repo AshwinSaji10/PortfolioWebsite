c0e2aa002f05dd81f0579c919a8d94e3401a19bf
AshwinSaji10 <aswinsaji777@gmail.com>
2024-07-08 15:43:25 +0530

    cleaned up files

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import "./footer.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Experience from "./components/Experience";
+// import About from "./components/About";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
+// import Experience from "./components/Experience";
 
 function App() {
     return (
@@ -17,10 +17,10 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     {/* <Route path='/Home' element={<Home/>}/> */}
-                    <Route path="/About" element={<About />} />
+                    {/* <Route path="/About" element={<About />} />
                     <Route path="/Projects" element={<Projects />} />
                     <Route path="/Contact" element={<Contact />} />
-                    <Route path="/Experience" element={<Experience />} />
+                    <Route path="/Experience" element={<Experience />} /> */}
                 </Routes>
                 <footer>
                     <div className="flex flex-row justify-between items-center bg-black px-3 py-3">
