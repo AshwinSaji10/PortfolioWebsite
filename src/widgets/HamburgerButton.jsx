7a5bd51fe90382da06771355491ab922281efba5
AshwinSaji10 <aswinsaji777@gmail.com>
2024-12-03 13:06:25 +0530

    hamburger button

---
+++ src/widgets/HamburgerButton.jsx
@@ -0,0 +1,16 @@
+import { motion, AnimatePresence } from "framer-motion";
+
+
+const HamburgerButton=()=>{
+
+    return(
+        <motion.button>
+            <motion.span style=
+            
+            />
+        </motion.button>
+    );
+
+}
+
+export default HamburgerButton;
